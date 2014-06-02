51d41e9928d46c04e2ea75c07c24c6c87335393d
Mattef <mattoef@yahoo.de>
2014-06-02 12:04:33 +0200

    Added copyright and licencing agreements

--- Vierteilchenzerfall/Minv.c
+++ Vierteilchenzerfall/Minv.c
@@ -1,3 +1,34 @@
+/*
+ Copyright (C) 2013-1024 Matthias Kesenheimer
+ m.kesenheimer@gmx.net
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ Dieses Programm ist Freie Software: Sie können es unter den Bedingungen
+ der GNU General Public License, wie von der Free Software Foundation,
+ Version 3 der Lizenz oder (nach Ihrer Wahl) jeder neueren
+ veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ 
+ Dieses Programm wird in der Hoffnung, dass es nützlich sein wird, aber
+ OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ Siehe die GNU General Public License für weitere Details.
+ 
+ Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ */
+
 //hier quadriertes Matrixelement in Abhängigkeit von eps, lam, s12, s23 und den Formfaktoren eingeben:
 
 double getMinv(double eps, double lam, double s12, double s34, double xsi, double tht, double phi, double F1, double F23, double F4, double F5, double F6) {
